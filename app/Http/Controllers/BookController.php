2026d96c085a8dbd8f14df559c14531b995c8c36
Diego Camacho <camachojcd@gmail.com>
2024-10-12 01:54:41 -0500

    Update book controller with NYT api service

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\BestSellerRequest;
+use App\Services\ApiInterface;
+use App\Services\NewYorkTimesApi;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
+    protected ApiInterface $api;
+
+    public function __construct(NewYorkTimesApi $api)
+    {
+        $this->api = $api;
+    }
+
     public function bestSellers(BestSellerRequest $request)
     {
-        return $request;
+        $params['author'] = $request->input('author');
+        $params['title'] = $request->input('title');
+        $params['isbns'] = $request->input('isbn');
+        $params['offset'] = $request->input('offset', 0);
+
+        $response = $this->api->fetchData($params);
+
+        return $response;
     }
 }
