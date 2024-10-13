d0568c5f66966909b8ec95da606e989a821c17f0
Diego Camacho <camachojcd@gmail.com>
2024-10-13 01:51:54 -0500

    Remove useless use

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 
