e2deb3d3390d8b1b159ab0f3f5f201c7817b71f1
Diego Camacho <camachojcd@gmail.com>
2024-10-13 21:33:54 -0500

    Separate api data conversion into method

--- app/Services/NewYorkTimesApi.php
+++ app/Services/NewYorkTimesApi.php
@@ -8,26 +8,35 @@ class NewYorkTimesApi implements ApiInterface
 {
     public function fetchData(array $params): array
     {
+        $apiParams = $this->convertData($params);
+
         $apiParams['api-key'] = config('services.nyt.key');
-
-        if (isset($params['author'])) {
-            $apiParams['author'] = $params['author'];
-        }
-
-        if (isset($params['isbns'])) {
-            $apiParams['isbn'] = implode(';', $params['isbns']);
-        }
-
-        if (isset($params['title'])) {
-            $apiParams['title'] = $params['title'];
-        }
-
-        if (isset($params['offset'])) {
-            $apiParams['offset'] = $params['offset'];
-        }
 
         $response = Http::get(config('services.nyt.url'), $apiParams);
 
         return $response->json();
+    }
+
+    public function convertData(array $params): array
+    {
+        $result = [];
+
+        if (isset($params['author'])) {
+            $result['author'] = $params['author'];
+        }
+
+        if (!empty($params['isbns'])) {
+            $result['isbn'] = implode(';', $params['isbns']);
+        }
+
+        if (isset($params['title'])) {
+            $result['title'] = $params['title'];
+        }
+
+        if (isset($params['offset'])) {
+            $result['offset'] = $params['offset'];
+        }
+
+        return $result;
     }
 }
