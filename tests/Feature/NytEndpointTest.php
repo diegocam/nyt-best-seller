12354663f286939f77563c6d165e973aa0302c88
Diego Camacho <camachojcd@gmail.com>
2024-10-13 23:26:17 -0500

    Add json api response structure test

--- tests/Feature/NytEndpointTest.php
+++ tests/Feature/NytEndpointTest.php
@@ -143,4 +143,86 @@ class NytEndpointTest extends TestCase
 
         ];
     }
+
+    public function testNytBestSellersEndpointResponse(): void
+    {
+        $response = $this->get('/api/1/nyt/best-sellers');
+        $response->assertJson([
+            'jsonapi' =>  [
+                'version' => '1.1'
+            ],
+            'data' => [
+                [
+                    'type' => 'books',
+                    'id' => 'ac6e70b49143a664b471aed484a0c2b7',
+                    'attributes' => [
+                        'title' => '"I GIVE YOU MY BODY ..."',
+                        'description' => 'The author of the Outlander novels gives tips on writing sex scenes, drawing on examples from the books.',
+                        'author' => 'Diana Gabaldon',
+                        'publisher' => 'Dell',
+                        'isbns' =>  [
+                            [
+                                'isbn10' => '0399178570',
+                                'isbn13' => '9780399178573',
+                            ]
+                        ]
+                    ],
+                    'relationships' => [
+                        'ranks_history' =>  [
+                            'data' => [
+                                [
+                                    'primary_isbn10' => '0399178570',
+                                    'primary_isbn13' => '9780399178573',
+                                    'rank' => 8,
+                                    'list_name' => 'Advice How-To and Miscellaneous',
+                                    'display_name' => 'Advice, How-To & Miscellaneous',
+                                    'published_date' => '2016-09-04',
+                                    'bestsellers_date' => '2016-08-20',
+                                    'weeks_on_list' => 1,
+                                    'rank_last_week' => 0,
+                                    'asterisk' => 0,
+                                    'dagger' => 0,
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                [
+                    'type' => 'books',
+                    'id' => 'a6f9b920ae122c723eab3c43f6f5752f',
+                    'attributes' =>  [
+                        'title' => '"MOST BLESSED OF THE PATRIARCHS"',
+                        'description' => 'A character study that attempts to make sense of Jefferson’s contradictions.',
+                        'author' => 'Annette Gordon-Reed and Peter S Onuf',
+                        'publisher' => 'Liveright',
+                        'isbns' =>  [
+                            [
+                                'isbn10' => '0871404427',
+                                'isbn13' => '9780871404428',
+                            ]
+                        ]
+                    ],
+                    'relationships' =>  [
+                        'ranks_history' => [
+                            'data' => [
+                                [
+                                    'primary_isbn10' => '0871404427',
+                                    'primary_isbn13' => '9780871404428',
+                                    'rank' => 16,
+                                    'list_name' => 'Hardcover Nonfiction',
+                                    'display_name' => 'Hardcover Nonfiction',
+                                    'published_date' => '2016-05-01',
+                                    'bestsellers_date' => '2016-04-16',
+                                    'weeks_on_list' => 1,
+                                    'rank_last_week' => 0,
+                                    'asterisk' => 1,
+                                    'dagger' => 0,
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
 }
